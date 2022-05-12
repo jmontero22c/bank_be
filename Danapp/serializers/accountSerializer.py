0255fdd6dad356e09b2263a812e2928e7c517812
jmontero22c <jesusda71.2@gmail.com>
2022-05-12 17:37:42 -0500

    first commit

---
+++ Danapp/serializers/accountSerializer.py
@@ -0,0 +1,7 @@
+from Danapp.models.account import Account
+from rest_framework import serializers
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        field = ['balance','lastChangeDate','isActive']
