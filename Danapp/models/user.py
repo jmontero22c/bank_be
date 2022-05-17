d77f2fed9501b9f3e115130f7264cf50965a59ae
jmontero22c <jesusda71.2@gmail.com>
2022-05-17 15:11:59 -0500

    chage_1

--- Danapp/models/user.py
+++ Danapp/models/user.py
@@ -1,5 +1,3 @@
-import email
-from tkinter.tix import Tree
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.contrib.auth.hashers import make_password
